fa033be438ab8fbadcf0947af9f92a30b2ac945b
neliton-andrade <andrade.neliton@escola.pr.gov.br>
2024-08-12 15:53:20 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+nd("white");
+}
+
+
+function draw() {
+  
+  stroke ("blue");
+  fill ("red");
+ 
+  
+  // console.log(mouseIsPressed);
+  
+  if (mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
